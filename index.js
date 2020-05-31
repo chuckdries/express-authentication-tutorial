efb36ae2933e1752c3f9c4a7d50bee3e965befc7
Chuck Dries <chuck@chuckdries.com>
2020-05-31 14:19:26 -0700

    add templating

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
 
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("home");
 });
 
 app.listen(3000, () => {
