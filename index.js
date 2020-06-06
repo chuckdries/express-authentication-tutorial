de33e1402feede2c1713111a5483755351b1ee61
Chuck Dries <chuck@chuckdries.com>
2020-06-06 10:02:00 -0700

    Finish adding templates and taking input from the user

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const app = express();
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.use(express.urlencoded());
+
+const messages = ["hello world"];
+
+function getHome(request, response) {
+  response.render("home", {
+    time: new Date().toLocaleTimeString(),
+    messages,
+  });
+}
+
+function receiveMessage(request, response) {
+  messages.push(request.body.message);
+  console.log(messages);
+  response.redirect("/");
+}
+
+app.get("/", getHome);
+
+app.post("/", receiveMessage);
 
 app.listen(3000, () => {
   console.log("Express auth tutorial running on http://localhost:3000");
---
+++ notes.md
@@ -0,0 +1,1 @@
+would you rather watch a syntax and language tutorial first, or watch a video that dives into language and application at the same time
