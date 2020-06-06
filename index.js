3ea2507c2139b8276718d9c8928e8058540b7ecb
Chuck Dries <chuck@chuckdries.com>
2020-06-06 10:21:09 -0700

    Store messages in database

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
+const db = require("better-sqlite3")("data.db");
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 app.use(express.urlencoded());
 
-const messages = ["hello world"];
-
 function getHome(request, response) {
+  const messages = db.prepare("SELECT * FROM messages").all();
   response.render("home", {
     time: new Date().toLocaleTimeString(),
     messages,
@@ -17,7 +17,10 @@ function getHome(request, response) {
 }
 
 function receiveMessage(request, response) {
-  messages.push(request.body.message);
+  db.prepare(`INSERT INTO messages (message) VALUES (?) `).run(
+    request.body.message
+  );
+  const messages = db.prepare("SELECT * FROM messages").all();
   console.log(messages);
   response.redirect("/");
 }
@@ -25,6 +28,20 @@ function receiveMessage(request, response) {
 app.get("/", getHome);
 
 app.post("/", receiveMessage);
+
+const setupDatabase = () => {
+  console.log(db);
+  db.prepare(
+    `CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY,
+    message STRING
+  );`
+  ).run();
+  const messages = db.prepare("SELECT * FROM messages").all();
+  console.log(messages);
+};
+
+setupDatabase();
 
 app.listen(3000, () => {
   console.log("Express auth tutorial running on http://localhost:3000");
